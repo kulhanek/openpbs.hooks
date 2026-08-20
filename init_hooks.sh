c24b06a30117740a874276c70313aebd2da62e90
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-08-20 22:20:44 +0200

    sync with current hooks

--- init_hooks.sh
+++ init_hooks.sh
@@ -3,10 +3,12 @@
 for HOOK in hook_discovery_node.qmgr  \
             hook_discovery_cpus.qmgr  \
             hook_discovery_gpus.qmgr  \
+            hook_discovery_interconnect.qmgr  \
+            hook_job_enqueued.qmgr  \
             hook_job_cgroups_v2.qmgr  \
             hook_job_gpus.qmgr  \
-            hook_job_env.qmgr  \
-            hook_workspace.qmgr; do
+            hook_workspace.qmgr \
+            hook_job_env.qmgr; do
     echo ""
     echo "# HOOK: $(basename $HOOK)"
     qmgr < $HOOK
