97fc7e15c12d836c3a01f31c0a1644b5b0b5af76
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-08-21 13:01:01 +0200

    initial version of discovery_containers

--- init_hooks.sh
+++ init_hooks.sh
@@ -4,6 +4,7 @@ for HOOK in hook_discovery_node.qmgr  \
             hook_discovery_cpus.qmgr  \
             hook_discovery_gpus.qmgr  \
             hook_discovery_interconnect.qmgr  \
+            hook_discovery_containers.qmgr  \
             hook_job_enqueued.qmgr  \
             hook_job_cgroups_v2.qmgr  \
             hook_job_gpus.qmgr  \
