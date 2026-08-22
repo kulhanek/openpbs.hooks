e314d3cf1a3fdfdc46c229231b150d547c3c5c61
Petr Kulhanek <kulhanek@chemi.muni.cz>
2026-08-22 17:59:21 +0200

    new hooks added

--- init_hooks.sh
+++ init_hooks.sh
@@ -5,7 +5,9 @@ for HOOK in hook_discovery_node.qmgr  \
             hook_discovery_gpus.qmgr  \
             hook_discovery_interconnect.qmgr  \
             hook_discovery_containers.qmgr  \
+            hook_aggregate_resources.qmgr  \
             hook_normalize_job_mpiomp.qmgr  \
+            hook_normalize_job_gpucap.qmgr  \
             hook_job_cgroups_v2.qmgr  \
             hook_job_gpus.qmgr  \
             hook_workspace.qmgr \
